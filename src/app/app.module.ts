ae75a1269a0d43ccceab52a560f50e6bc9adcbd8
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2020-12-24 09:21:42 +0530

    material
    added material dedign

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatIconModule} from '@angular/material/icon';
 import { OptionsComponent } from './options/options.component';
 import { hostViewClassName } from '@angular/compiler';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,10 @@ import { hostViewClassName } from '@angular/compiler';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
