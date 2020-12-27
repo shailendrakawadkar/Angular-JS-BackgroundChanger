1825b5138cb101145c4a2ccfe885a8b272a0661b
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2020-12-27 18:04:38 +0530

    Complete
    successful completed basic project.

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare  var jQuery:  any;
 @Component({
   selector: 'app-options',
   templateUrl: './options.component.html',
@@ -10,6 +10,34 @@ export class OptionsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    (function ($) {
 
+      $(".day").click(() => {
+        $("body").css("background-color", "Yellow");
+        $("h1").css("color","black");
+        $("h3").css("color","black");
+      });
+      $(".night").click(() => {
+        $("body").css("background-color", "black");
+        $("h1").css("color","white");
+        $("h3").css("color","white");
+      });
+      $(".mint").click(() => {
+        $("body").css("background-color", "green");
+        $("h1").css("color","white");
+        $("h3").css("color","white");
+      });
+      $(".voilet").click(() => {
+        $("body").css("background-color", "blueviolet");
+        $("h1").css("color","white");
+        $("h3").css("color","white");
+      });
+      $(".sensation").click(() => {
+        $("body").css("background-color", "red");
+        $("h1").css("color","white");
+        $("h3").css("color","white");
+      });
+    })(jQuery);
+  }
+  
 }
